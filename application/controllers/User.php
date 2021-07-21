628e537d3282c28ce6e5a6401887c56a75a7c917
taras <moroz.tara.box@gmail.com>
2021-07-21 12:58:12 +0300

    rest files added

---
+++ application/controllers/User.php
@@ -0,0 +1,48 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+	private $device = 'desktop';
+	private $viewfolder = 'desktop';
+
+	public function __construct()
+ 	{
+ 		
+ 		parent::__construct();
+		$this->load->helper('url');
+
+		if($this->agent->is_mobile()) $this->device =  'mobile';
+ 	}
+
+	public function index()
+ 	{
+		header("HTTP/1.0 404 Not Found");
+
+		$data = get_common_page_data();
+		$this->load->view($this->viewfolder.'/v_error-404', $data);
+ 	}
+
+
+ 	public function login(){
+
+		$data = get_common_page_data();
+
+		$this->load->view($this->viewfolder.'/v_login', $data);
+	}
+
+ 	public function signup(){
+
+		$data = get_common_page_data();
+
+		$this->load->view($this->viewfolder.'/v_signup', $data);
+	}
+
+ 	public function account(){
+
+		$data = get_common_page_data();
+
+		$this->load->view($this->viewfolder.'/v_account', $data);
+	}
+	
+}
